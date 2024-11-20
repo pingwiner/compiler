a2b5f70eeaae4e27d9901810b566562fc917c3ba
pingwiner <i486dx@yandex.ru>
2024-11-21 02:02:54 +0400

    Lexer test

--- src/test/kotlin/LexerTest.kt
+++ src/test/kotlin/LexerTest.kt
@@ -55,5 +55,51 @@ class LexerTest {
         assertIs<Number>(lexer.tokens[10])
         assertIs<SpecialSymbol.End>(lexer.tokens[11])
         assertIs<SpecialSymbol.RCurl>(lexer.tokens[12])
+
+        lexer.reset()
+        lexer.scan("a[i] = 1 + 2/3 - 4 *6 & (c% d) | 0 ^ 3;")
+        assertEquals(25, lexer.tokens.size)
+
+        assertIs<Symbol>(lexer.tokens[0])
+        assertIs<SpecialSymbol.LSquare>(lexer.tokens[1])
+        assertIs<Symbol>(lexer.tokens[2])
+        assertIs<SpecialSymbol.RSquare>(lexer.tokens[3])
+        assertIs<Operator.Plus>(lexer.tokens[6])
+        assertIs<Operator.Divide>(lexer.tokens[8])
+        assertIs<Operator.Minus>(lexer.tokens[10])
+        assertIs<Operator.Multiply>(lexer.tokens[12])
+        assertIs<Operator.And>(lexer.tokens[14])
+        assertIs<Operator.Mod>(lexer.tokens[17])
+        assertIs<Operator.Or>(lexer.tokens[20])
+        assertIs<Operator.Xor>(lexer.tokens[22])
+
+        lexer.reset()
+        lexer.scan("a = ((x > 1) | (y < 2)) ? 1 : 2;")
+        assertEquals(20, lexer.tokens.size)
+
+        assertIs<SpecialSymbol.LBrace>(lexer.tokens[2])
+        assertIs<SpecialSymbol.LBrace>(lexer.tokens[3])
+        assertIs<Operator.Gt>(lexer.tokens[5])
+        assertIs<SpecialSymbol.RBrace>(lexer.tokens[7])
+        assertIs<Operator.Lt>(lexer.tokens[11])
+        assertIs<Operator.If>(lexer.tokens[15])
+        assertIs<Operator.Else>(lexer.tokens[17])
+
+        lexer.reset()
+        lexer.scan("{x=x+1;} while (x <= 10); {x=x-1;} until (x>=0);")
+        assertEquals(30, lexer.tokens.size)
+
+        assertIs<Operator.While>(lexer.tokens[8])
+        assertIs<Operator.Until>(lexer.tokens[23])
+        assertIs<Operator.LtEq>(lexer.tokens[11])
+        assertIs<Operator.GtEq>(lexer.tokens[26])
+
+        lexer.reset()
+        lexer.scan("(a != 2) ? a << 1 : a >> 2;")
+        assertEquals(14, lexer.tokens.size)
+
+        assertIs<Operator.Neq>(lexer.tokens[2])
+        assertIs<Operator.Shl>(lexer.tokens[7])
+        assertIs<Operator.Shr>(lexer.tokens[11])
     }
 }
